f7c74349a07eb8a1e42235fcb6cd128f21197f81
Luuk de Waal Malefijt <luuk@positivemoney.org>
2016-07-15 12:48:11 +0200

    download version of mbslave on build

--- image/volume/mbslave2/sql/wikidocs/DropPrimaryKeys.sql
+++
@@ -1,6 +0,0 @@
--- Automatically generated, do not edit.
-\unset ON_ERROR_STOP
-
-SET search_path = 'wikidocs';
-
-ALTER TABLE wikidocs_index DROP CONSTRAINT IF EXISTS wikidocs_index_pkey;
--- image/volume/mbslave2/sql/wikidocs/DropTables.sql
+++
@@ -1,6 +0,0 @@
--- Automatically generated, do not edit.
-\unset ON_ERROR_STOP
-
-SET search_path = 'wikidocs';
-
-DROP TABLE wikidocs_index;
